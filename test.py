17857b03cbc0eb2e548ceeacfc4dc4cbe6042ae9
Tyler <tyler.christian2@gmail.com>
2016-03-28 16:34:02 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def do_it_work_man():
-  this_tab_looks_like_a_space=0
-  if this_tab_looks_like_a_space==0:
-    print "this shit works!"
-  else:
-    print "this is fucking lame...I suck"
-
-do_it_work_man()
